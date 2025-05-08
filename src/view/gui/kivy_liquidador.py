46be27be214ad0bbede085777df156733399e3d9
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-07 21:13:43 -0500

    actualizacion del import

--- src/view/gui/kivy_liquidador.py
+++ src/view/gui/kivy_liquidador.py
@@ -1,8 +1,9 @@
-from Main_screen import MainScreen
-from Second_screen import SecondScreen
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
+
+from .Main_screen import MainScreen
+from .Second_screen import SecondScreen
+
 
 class liquidadorLaboral(App):
     def build(self):
@@ -10,6 +11,3 @@ class liquidadorLaboral(App):
         sm.add_widget(MainScreen(name = "main"))
         sm.add_widget(SecondScreen(name = "second"))
         return sm
-
-if __name__ == "__main__":
-    liquidadorLaboral().run()
